51cdd29033f633a2ecfce046e1900bb24f0667fb
cesaraugusto87 <cesars@kipo.co>
2017-04-21 15:00:14 -0600

    Initial Commit

---
+++ Catalogo Pockemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catalogo Pockemon
+//
+//  Created by Kipo on 4/21/17.
+//  Copyright © 2017 Kipo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
